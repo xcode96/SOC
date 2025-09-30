5d97c8b47251697a87e5780eaa13e931c1259cc9
Mani Bharathi P <charuxdq@gmail.com>
2025-09-30 22:10:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/SOC/',   // MUST match repo name
+  base: '/SOC/',   // ⚠️ Important for GitHub Pages
   plugins: [react()],
 })
